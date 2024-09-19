ad5fdd319e4e23f2bda6a6a47ed6563f000db789
chainosaur1214 <maribethmaca@gmail.com>
2024-09-19 11:20:55 +0800

    finalyzing script

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Number_Guessing_Game
+# Number Guessing Game
+
+Welcome to the Number Guessing Game! This is a CLI-based game where the user has to guess a number between 1 and 100. The user has a limited number of chances based on the difficulty level to guess the correct number.
+
+## How to Play
+--------------
+
+1. Run the game by executing the python file.
+2. You will be presented with a welcome message and a prompt to select the difficulty level.
+3. Choose the difficulty level by entering 1 for Easy, 2 for Medium, or 3 for Hard.
+4. Once you've selected the difficulty level, you will be prompted to enter your guess.
+5. Enter a number between 1 and 100.
+6. If your guess is incorrect, you will receive a hint indicating whether the correct number is higher or lower than your guess.
+7. Keep guessing until you correctly guess the number or run out of attempts. 
+8. User to play multiple rounds of the game. Program will ask the user if they want to play again after each round.
+
+## Difficulty Levels
+-------------------
+
+* **Easy**: 10 attempts to guess the correct number.
+* **Medium**: 5 attempts to guess the correct number.
+* **Hard**: 3 attempts to guess the correct number.
--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,5 +1,6 @@
 import sys
 from random import randrange
+from datetime import datetime as d
 
 difficulty_level = ({'choice': 1, 'diff_lvl': 'EASY', 'chances': 10}, {'choice': 2, 'diff_lvl': 'MEDIUM', 'chances': 5}, {'choice': 3, 'diff_lvl': 'HARD', 'chances': 3})
 def rules():
@@ -9,41 +10,76 @@ def rules():
 		print(f"{level['choice']}. {level['diff_lvl']} ({level['chances']} chances)")
 
 def choose_lvl(lvl):
-	for level in difficulty_level:
-		if int(level['choice']) == int(lvl):
-			return level['diff_lvl'], level['chances']
-			break
+	try:
+		lvl = int(lvl)
+		found = False
+		for level in difficulty_level:
+			if int(level['choice']) == int(lvl):
+				found = True
+				return level['diff_lvl'], level['chances']
+		if not found:
+			print("Invalid choice. Enter only integer. 1-3")
+			return None
+	except ValueError:
+		print("Invalid choice. Enter only integer. 1-3")
+		return None
 
 def get_rand_number()->int:
 	return randrange(100)
 
+def check_y_n(val)-> object:
+	if val == 'Y' or val == 'N' or val == 'y' or val == 'n':
+		return val.upper()
+	else:
+		print("Enter only Y/N")
+		return None
+
+def check_num(val)-> int:
+	try:
+		num = int(val)
+		if num < 100 and num > 0:
+			return num
+		else:
+			print("Select a number between 1 and 100.")
+			return None
+	except ValueError:
+		print("Enter only integer.")
+		return None
 
 def main():
-	# Check if the script is run standalone
 	if len(sys.argv) >= 1:
 		rules()
 		num = get_rand_number()
-		choice = input("Enter your choice: ")
-		result = choose_lvl(choice)
-		print(num)
-		if result:
-			difficulty, chances = result
-			print(f"Great! You have selected the {difficulty} difficulty level. You have {chances} chances to guess.\nLet's start the game!")
-			attempt=0
-			while(attempt<=chances):				
-				attempt+=1
-				print(F'{attempt} of {chances}')
-				guess = input("Enter your guess: ")
-				
-				if int(guess) > num:
-					print(f"Incorrect! The number is greater than {guess}.")
-				elif int(guess) < num:
-					print(f"Incorrect! The number is less than {guess}.")
-				elif int(guess) == num:
-					print(f"Congratulations! You guessed the correct number in {attempt} attempts.")
-					break
+		choice = None
+		result = None
+		while not result:
+			choice = input("Enter your choice: ")
+			result = choose_lvl(choice)
+		difficulty, chances = result
+		print(f"Great! You have selected the {difficulty} difficulty level. You have {chances} chances to guess.\nLet's start the game!")
+		time_start = d.now()
+		attempt=0
+		while(attempt<chances):				
+			attempt+=1
+			print(F'Attempt: ({attempt} of {chances})')
+			guess = None
+			while (not guess):
+				guess = check_num(input("Enter your guess: "))
+			if int(guess) > num:
+				print(f"Incorrect! The number is greater than {guess}.")
+			elif int(guess) < num:
+				print(f"Incorrect! The number is less than {guess}.")
+			elif int(guess) == num:
+				print(f"Congratulations! You guessed the correct number in {attempt} attempt(s).")
+				print(f"Completed in {d.now() - time_start}")
+				break
+		play_again = None
+		while not play_again:
+			play_again = check_y_n(input("Do you want to play again? (Y/N) "))
+		if play_again == 'Y':
+			main()
 		else:
-			print("Invalid choice")
+			print("Exiting. Thank you for playing")
 	else:
 		print("This script can also be used as a module by importing the process_numbers function.")
 
